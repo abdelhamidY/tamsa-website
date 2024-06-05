4b7fc901177410903a4f67672acce0be4d49d506
AbdelRhman Samy <abdojoey17@gmail.com>
2024-06-05 07:49:30 +0300

    .

--- components/home/Clients/Clients.tsx
+++ components/home/Clients/Clients.tsx
@@ -1,26 +1,96 @@
+"use client";
 import { CommunitySvgrepoCom, LeftArrowSvgrepoCom, Logo } from "@/icons";
 import Image from "next/image";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import { Pagination } from "swiper/modules";
+import { motion, useAnimation } from "framer-motion";
+
 const Clients = () => {
+	const slideChange = useAnimation();
+	const handleHoverStart = () => {
+		slideChange.start({
+		  x: 50, // Move text to the right
+		  transition: { type: "spring", stiffness: 150, damping: 10 },
+		});
+	  };
+	  const handleHoverEnd = () => {
+		slideChange.start({
+		  x: 0,
+		  transition: { type: "spring", stiffness: 150, damping: 10 },
+		});
+	  };
+	
   return (
     <div className="bg-[#fff] items-center justify-center flex flex-col px-10 py-10 rounded-lg  gap-8 md:h-[500px] my-20 ">
-			<div className="flex flex-col items-center justify-center text-center">
-			<h1 className="text-[29px]">عملاؤنا</h1>
-        <div className='w-[80px] h-1 bg-[#D57153] mt-2 mb-5'></div>
-			</div>
-      
-      <div   className="bg-[#fff] w-full lg:w-3/6 md:h-[400px] h-auto grid grid-cols-3 grid-rows-6  md:grid-cols-6 md:grid-rows-3 py-5 px-2 shadow-2xl items-center justify-center">
-				{Array.from({length: 18}).map((item, index) => {
-					return(
+      <div className="flex flex-col items-center justify-center text-center">
+        <h1 className="text-[29px]">عملاؤنا</h1>
+        <div className="w-[80px] h-1 bg-[#D57153] mt-2 mb-5"></div>
+      </div>
+      <Swiper
 
-				<div  className="flex items-center justify-center" key={index}>
-				<Image src={'/images/partners/ec181d1119f666b350beaf7ecced6fba.png'} alt="client" width={100} height={100}/>
-				</div>
-					)
-				})}
-				
+slidesPerView={1}
+        autoplay={{ delay: 3000 }}
+        pagination={{
+          enabled: false,
+        }}
 
-			</div>
+        onTransitionStart={handleHoverStart}
+        onTransitionEnd={handleHoverEnd}
+        className="bg-[#fff] w-full lg:w-3/6 h-auto grid grid-cols-3 grid-rows-6  md:grid-cols-6 md:grid-rows-3 py-5 px-2 shadow-2xl items-center justify-center"
+      >
+        <SwiperSlide>
+          <div  className="bg-[#fff] w-full lg:w-full  h-auto grid grid-cols-3 grid-rows-6  md:grid-cols-6 md:grid-rows-3 px-2 shadow-2xl items-center justify-center">
+            {Array.from({ length: 18 }).map((item, index) => {
+              return (
+                <motion.div animate={(index % 2)===0 ? slideChange:""} className="flex items-center justify-center" key={index}>
+                  <Image
+                    src={
+                      "/images/partners/ec181d1119f666b350beaf7ecced6fba.png"
+                    }
+                    alt="client"
+                    width={100}
+                    height={100}
+                  />
+                </motion.div>
+              );
+            })}
+          </div>
+        </SwiperSlide>
+		<SwiperSlide>
+          <motion.div className="bg-[#fff] w-full lg:w-full  h-auto grid grid-cols-3 grid-rows-6  md:grid-cols-6 md:grid-rows-3 px-2 shadow-2xl items-center justify-center">
+            {Array.from({ length: 18 }).map((item, index) => {
+              return (
+                <div className="flex items-center justify-center" key={index}>
+                  <Image
+                    src={
+                      "/images/partners/ec181d1119f666b350beaf7ecced6fba.png"
+                    }
+                    alt="client"
+                    width={100}
+                    height={100}
+                  />
+                </div>
+              );
+            })}
+          </motion.div>
+        </SwiperSlide>
+      </Swiper>
+      {/* <div className="bg-[#fff] w-full lg:w-3/6 md:h-[400px] h-auto grid grid-cols-3 grid-rows-6  md:grid-cols-6 md:grid-rows-3 py-5 px-2 shadow-2xl items-center justify-center">
+        {Array.from({ length: 18 }).map((item, index) => {
+          return (
+            <div className="flex items-center justify-center" key={index}>
+              <Image
+                src={"/images/partners/ec181d1119f666b350beaf7ecced6fba.png"}
+                alt="client"
+                width={100}
+                height={100}
+              />
+            </div>
+          );
+        })}
+      </div> */}
     </div>
   );
 };
--- components/home/Testimonials/Testimonials.tsx
+++ components/home/Testimonials/Testimonials.tsx
@@ -8,12 +8,34 @@ import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/bundle";
 import { Pagination } from "swiper/modules";
+import { motion, useAnimation } from "framer-motion";
 
 // const swiper = Swiper({
 // 	modules: [Pagination],
 // });
 
 const Testimonials = () => {
+  const slideChange = useAnimation();
+  const handleHoverStartR = () => {
+    slideChange.start({
+      x: 350, // Move text to the right
+      transition: { type: "spring", stiffness: 150, damping: 10 },
+    });
+  };
+
+  const handleHoverStartL = () => {
+    slideChange.start({
+      x: -350, // Move text to the right
+      transition: { type: "spring", stiffness: 150, damping: 10 },
+    });
+  };
+  const handleHoverEnd = () => {
+    slideChange.start({
+      x: 0,
+      transition: { type: "spring", stiffness: 150, damping: 10 },
+    });
+  };
+
   return (
     <div className="bg-[#fff] items-center justify-center flex flex-col px-10 py-10 rounded-lg h-auto gap-8 sm:h-[500px] my-20 ">
       <div className="flex flex-col items-center justify-center text-center">
@@ -29,53 +51,63 @@ const Testimonials = () => {
           enabled: true,
           clickable: true,
         }}
+        onSlideNextTransitionStart={handleHoverStartR}
+        onSlideNextTransitionEnd={handleHoverEnd}
+        onSlidePrevTransitionStart={handleHoverStartL}
+        onSlidePrevTransitionEnd={handleHoverEnd}
+        // onTransitionStart={}
+        // onTransitionEnd={handleHoverEnd}
         className="bg-[#fff]  w-full sm:w-[450px] h-auto sm:h-[350px] flex gap-5 flex-col py-10 px-10 shadow-2xl items-end justify-start"
       >
         <SwiperSlide className="bg-[#fff] w-full sm:w-[450px] h-auto sm:h-[350px] flex gap-5 flex-col py-10 px-10 shadow-2xl items-end justify-start">
-          <div className="flex items-center flex-row-reverse justify-end rounded-full gap-5 mb-2">
-            <h3 className="text-[23px] font-bold">أحمد أحمد</h3>
-            <Image
-              className="rounded-full h-[100px] w-[100px]"
-              src={"/images/partners/173a586731066381a669f9f2e893cb0b.jpg"}
-              alt="client"
-              width={100}
-              height={100}
-            />
-          </div>
-          <div className="w-full sm:w-[400px] h-auto  flex justify-end mb-2">
-            <p className="text-start">
-              كان لي الشرف بالتعاون مع شركة تام، ويجب أن أقول إن طاقمهم المتميز
-              وشراكاتهم المؤثرة في قطاع غير الربح تعكس التزامهم بالتغيير
-              الإيجابي
-            </p>
-          </div>
+          <motion.div animate={slideChange} className="">
+            <div className="flex items-center flex-row-reverse justify-end rounded-full gap-5 mb-2">
+              <h3 className="text-[23px] font-bold">أحمد أحمد</h3>
+              <Image
+                className="rounded-full h-[100px] w-[100px]"
+                src={"/images/partners/173a586731066381a669f9f2e893cb0b.jpg"}
+                alt="client"
+                width={100}
+                height={100}
+              />
+            </div>
+            <div className="w-full sm:w-[400px] h-auto  flex justify-end mb-2">
+              <p className="text-start">
+                كان لي الشرف بالتعاون مع شركة تام، ويجب أن أقول إن طاقمهم
+                المتميز وشراكاتهم المؤثرة في قطاع غير الربح تعكس التزامهم
+                بالتغيير الإيجابي
+              </p>
+            </div>
 
-          <div className="w-full sm:w-[400px] start justify-end mb-2">
-            <p className="text-start">15 فبراير 2023</p>
-          </div>
+            <div className="w-full sm:w-[400px] start justify-end mb-2">
+              <p className="text-start">15 فبراير 2023</p>
+            </div>
+          </motion.div>
         </SwiperSlide>
         <SwiperSlide className="bg-[#fff] w-full md:w-[450px] h-[450px] md:h-[350px] flex gap-5 flex-col py-10 px-10 shadow-2xl items-end justify-start">
-          <div className="flex items-center flex-row-reverse justify-end rounded-full gap-5 mb-2">
-            <h3 className="text-[23px] font-bold">أحمد أحمد</h3>
-            <Image
-              className="rounded-full h-[100px] w-[100px]"
-              src={"/images/partners/173a586731066381a669f9f2e893cb0b.jpg"}
-              alt="client"
-              width={100}
-              height={100}
-            />
-          </div>
-          <div className="w-full md:w-[400px] flex justify-end mb-2">
-            <p className="text-start">
-              كان لي الشرف بالتعاون مع شركة تام، ويجب أن أقول إن طاقمهم المتميز
-              وشراكاتهم المؤثرة في قطاع غير الربح تعكس التزامهم بالتغيير
-              الإيجابي
-            </p>
-          </div>
+          <motion.div animate={slideChange} className="">
+            <div className="flex items-center flex-row-reverse justify-end rounded-full gap-5 mb-2">
+              <h3 className="text-[23px] font-bold">أحمد أحمد</h3>
+              <Image
+                className="rounded-full h-[100px] w-[100px]"
+                src={"/images/partners/173a586731066381a669f9f2e893cb0b.jpg"}
+                alt="client"
+                width={100}
+                height={100}
+              />
+            </div>
+            <div className="w-full md:w-[400px] flex justify-end mb-2">
+              <p className="text-start">
+                كان لي الشرف بالتعاون مع شركة تام، ويجب أن أقول إن طاقمهم
+                المتميز وشراكاتهم المؤثرة في قطاع غير الربح تعكس التزامهم
+                بالتغيير الإيجابي
+              </p>
+            </div>
 
-          <div className="w-full md:w-[400px] start justify-end mb-2">
-            <p className="text-start">15 فبراير 2023</p>
-          </div>
+            <div className="w-full md:w-[400px] start justify-end mb-2">
+              <p className="text-start">15 فبراير 2023</p>
+            </div>
+          </motion.div>
         </SwiperSlide>
       </Swiper>
     </div>
